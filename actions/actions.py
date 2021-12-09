27f5e600c545b2b5f2113a822f743abbbb3e1322
ArianeDlns <ariane.dalens@student.ecp.fr>
2021-12-09 15:44:18 +0100

    Adding action_get_sondage_candidat
    Former-commit-id: 58af5f5f99ee8e1becf5d8fcd92dd135a4eac0cb

--- actions/actions.py
+++ actions/actions.py
@@ -11,6 +11,8 @@ from typing import Any, Text, Dict, List
 from pathlib import Path
 import json
 
+from utils.scrapping_sondages import *
+
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
@@ -24,18 +26,25 @@ class ActionGetCandidates(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-# .encode("latin_1").decode("utf_8")
-        candidates_name = [candidate['name'] for candidate in self.candidates_data['candidates']]
-        candidates_party = [candidate['party'] for candidate in self.candidates_data['candidates']]
-        candidates_name_party = [f"{candidates_name[i]} ({candidates_party[i]})\n" for i in range(len(candidates_name))]
-        dispatcher.utter_message(text=f"Voici la liste des candidats participants aux élections présidentielles de 2022 :\n- {'- '.join(candidates_name_party)}")
+        # .encode("latin_1").decode("utf_8")
+        candidates_name = [candidate['name']
+                           for candidate in self.candidates_data['candidates']]
+        candidates_party = [candidate['party']
+                            for candidate in self.candidates_data['candidates']]
+        candidates_name_party = [
+            f"{candidates_name[i]} ({candidates_party[i]})\n" for i in range(len(candidates_name))]
+        dispatcher.utter_message(
+            text=f"Voici la liste des candidats participants aux élections présidentielles de 2022 :\n- {'- '.join(candidates_name_party)}")
 
         return []
 
+
 class ActionGetPartyFromCandidate(Action):
     candidates_data = json.loads(Path("data/candidates.json").read_text())
-    candidates_name = [candidate['name'] for candidate in candidates_data['candidates']]
-    candidates_party = [candidate['party'] for candidate in candidates_data['candidates']]
+    candidates_name = [candidate['name']
+                       for candidate in candidates_data['candidates']]
+    candidates_party = [candidate['party']
+                        for candidate in candidates_data['candidates']]
 
     def name(self) -> Text:
         return "action_get_party_from_candidate"
@@ -48,9 +57,44 @@ class ActionGetPartyFromCandidate(Action):
             if blob['entity'] == 'candidate_name':
                 name = blob['value']
                 if name in self.candidates_name:
-                    party = self.candidates_party[self.candidates_name.index(name)]
-                    dispatcher.utter_message(text=f"Le parti politique de {name} est {party}")
+                    party = self.candidates_party[self.candidates_name.index(
+                        name)]
+                    dispatcher.utter_message(
+                        text=f"Le parti politique de {name} est {party}")
                 else:
-                    dispatcher.utter_message(text=f"Je ne reconnais pas le nom de ce candidat. L'avez-vous bien écrit ?")
-                
+                    dispatcher.utter_message(
+                        text=f"Je ne reconnais pas le nom de ce candidat. L'avez-vous bien écrit ?")
+
+        return []
+
+
+class ActionGetPartyFromCandidate(Action):
+    candidates_data_sondage = get_sondages(
+        "https://fr.wikipedia.org/wiki/Liste_de_sondages_sur_l%27%C3%A9lection_pr%C3%A9sidentielle_fran%C3%A7aise_de_2022")
+    candidates_data = json.loads(Path("data/candidates.json").read_text())
+    candidates_name = [candidate['name']
+                       for candidate in candidates_data['candidates']]
+    candidates_party = [candidate['party']
+                        for candidate in candidates_data['candidates']]
+
+    def name(self) -> Text:
+        return "action_get_sondage_candidat"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        for blob in tracker.latest_message['entities']:
+
+            if blob['entity'] == 'candidate_name':
+                name = blob['value']
+                if name in self.candidates_name:
+                    name_value = name.split(
+                        ' ')[-1] + "(" + self.candidates_party[self.candidates_name.index(name)] + ")"
+                    poll_value = self.candidates_data_sondage.iloc[0][name_value]
+                    dispatcher.utter_message(
+                        text=f"{name} est à {poll_value} dans le dernier sondage")
+                else:
+                    dispatcher.utter_message(text=f"Je ne reconnais pas le nom de ce candidat. L'avez-vous bien écrit ? \n Les candidats sont:" + (
+                        f"({self.candidates_name[i]})\n" for i in range(len(self.candidates_name))))
         return []
